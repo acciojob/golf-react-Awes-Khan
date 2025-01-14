26a3d6e97b3b79e15b0986462ce565595e4f258c
Awes Khan <44608315+Awes-Khan@users.noreply.github.com>
2025-01-14 22:49:14 +0530

    Golf Functionality Implemented

--- src/components/App.js
+++ src/components/App.js
@@ -11,22 +11,55 @@ class App extends Component {
         };
         this.renderChoice = this.renderBallOrButton.bind(this)
         this.buttonClickHandler = this.buttonClickHandler.bind(this)
+        this.keyDownHandler = this.keyDownHandler.bind(this);
     };
 
     buttonClickHandler() {
+        this.setState({
+          renderBall:true,
+          posi : 0,
+          ballPosition: { left: "0px" }
+        });
    
    }
     renderBallOrButton() {
 		if (this.state.renderBall) {
 		    return <div className="ball" style={this.state.ballPosition}></div>
 		} else {
-		    return <button onClick={this.buttonClickHandler} >Start</button>
+		    return <button className="start" onClick={this.buttonClickHandler} >Start</button>
 		}
+    }
+
+    keyDownHandler(e){
+        if(event.key === "ArrowLeft" || e.keyCode === 37){
+            this.setState((prevState)=>{
+                return  {
+                    posi : prevState.posi - 5,
+                    ballPosition: { left: prevState.posi-5 + "px" }
+                }
+                // posi : this.state.posi - 5,
+                // ballPosition: { left: this.state.posi + "px" }
+              });
+            // console.log("37",this.state.posi);
+        } else if(event.key === "ArrowRight" || e.keyCode === 39){
+            this.setState((prevState)=>{
+                return  {
+                    posi : prevState.posi + 5,
+                    ballPosition: { left: prevState.posi+5 + "px" }
+                }
+                // posi : this.state.posi + 5,
+                // ballPosition: { left: this.state.posi + 5 + "px" }
+              });
+        }
     }
 
     // bind ArrowRight keydown event
     componentDidMount() {
+        document.addEventListener("keydown", this.keyDownHandler); 
       
+    }
+    componentWillUnmount() {
+        document.removeEventListener("keydown", this.keyDownHandler);
     }
 
     render() {
